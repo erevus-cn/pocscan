5e4c4b31564b6842d29cf17d6c53c94fe35c040f
zhangzhe <zhangzhe@bilibili.com>
2016-04-12 15:47:40 +0800

    爬虫结果保存进数据库

--- web/models.py
+++ web/models.py
@@ -1,19 +1,34 @@
 from django.db import models
 
+
 class Result(models.Model):
     domain = models.TextField()
     poc_file = models.TextField()
     result = models.TextField()
-    date =  models.DateTimeField(auto_now_add=True, blank=True)
-    is_fixed = models.NullBooleanField (default=False)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+    is_fixed = models.NullBooleanField(default=False)
 
     def __unicode__(self):
         return self.result
 
+
 class Tasks_status(models.Model):
     domains = models.TextField()
     task_name = models.TextField()
-    status = models.NullBooleanField (default=False)
+    status = models.NullBooleanField(default=False)
 
     def __unicode__(self):
-         return self.domains
+        return self.domains
+
+
+class Req_list(models.Model):
+    method = models.CharField('METHOD', max_length=5, )
+    host = models.CharField('HOST', max_length=40, )
+    uri = models.CharField('FILE', max_length=100, default='/', )
+    url = models.TextField('URL', )
+    referer = models.TextField('REFERER', null=True)
+    data = models.TextField('REQUEST BODY', null=True)
+    cookie = models.TextField('COOKIE', default='', )
+
+    def __self__(self):
+        return self.url
--- web/tasks.py
+++ web/tasks.py
@@ -5,20 +5,24 @@ from web.lib.utils import *
 from web.lib.crawler import MyCrawler, similarity
 from pocscan.poc_launcher import Poc_Launcher
 from celery import Celery, platforms, task
+from .models import Req_list
+from urlparse import urlparse
+
 
 # 失败任务重启休眠时间300秒，最大重试次数5次
-#@app.task(bind=True, default_retry_delay=300, max_retries=5)
+# @app.task(bind=True, default_retry_delay=300, max_retries=5)
 @task(time_limit=3600)
-def run_task_in_gevent(url_list, poc_file_dict):     # url_list 每个进程分配到一定量的url
+def run_task_in_gevent(url_list, poc_file_dict):  # url_list 每个进程分配到一定量的url
     poc = Poc_Launcher()
     pool = Pool(100)
     for target in url_list:
-        for plugin_type,poc_files in poc_file_dict.iteritems():
+        for plugin_type, poc_files in poc_file_dict.iteritems():
             for poc_file in poc_files:
                 if target and poc_file:
                     target = fix_target(target)
                     pool.add(gevent.spawn(poc.poc_verify, target, plugin_type, poc_file))
     pool.join()
+
 
 @task(time_limit=3600)
 def crawler(target, cookie, ua):
@@ -26,13 +30,23 @@ def crawler(target, cookie, ua):
     crawl_count = 5
     craw = MyCrawler(target, cookie, ua)
     craw.crawling(target, crawl_count)
-    url_list = list(set( craw.linkQuence.getUnvisitedUrl()+craw.linkQuence.getVisitedUrl() ))
+    url_list = list(set(craw.linkQuence.getUnvisitedUrl() + craw.linkQuence.getVisitedUrl()))
     size = 1000000000
     for url in url_list:
         try:
-            temp = { str(similarity(url,size)): url}
+            temp = {str(similarity(url, size)): url}
             result.update(temp)
         except Exception, e:
             print e
-    # 这里存数据库
+    try:
+        for url in result.values():
+            tmp = urlparse(url)
+            Req_list(method="GET",
+                     host=tmp.netloc,
+                     uri=tmp.path,
+                     url=url.encode("utf8"),
+                     cookie=cookie,
+                     ).save()
+    except Exception, e:
+        pass
     return result
