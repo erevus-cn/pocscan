2c44f70b69ec98b23e2972adaf2c6f9b775e35e4
tlskbz <admin@admin.com>
2016-04-15 11:54:32 +0800

    删除表格中的排序

--- web/templatetags/sploit.py
+++ web/templatetags/sploit.py
@@ -1,11 +1,13 @@
 import re
 from django import template
+
 register = template.Library()
+
 
 @register.filter(name="sp")
 def sp(pocpath):
     try:
         pocname = re.findall(r"/(\w+)\.py", pocpath)[0]
-    except Exception,e:
+    except Exception, e:
         pocname = "LOAD ERROR"
     return pocname
